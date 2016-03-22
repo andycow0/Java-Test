e527de4cbdecc9fdb7d2975498cceecf3f4a8582
andycow0 <andycow0@gmail.com>
2016-03-23 00:26:12 +0800

    Can application/json method for Jersey2RESfulExample.

---
+++ workspace/RESTfulWS/src/com/eviac/blog/Customer.java
@@ -0,0 +1,25 @@
+package com.eviac.blog;
+
+//import javax.xml.bind.annotation.XmlRootElement;
+
+//@XmlRootElement
+public class Customer {
+	private String _Name;
+
+	public Customer() {
+
+	}
+
+	public String get_Name() {
+		return _Name;
+	}
+
+	public void set_Name(String _Name) {
+		this._Name = _Name;
+	}
+    @Override
+    public String toString() {
+        return "JsonExample [title=" + get_Name() + "]";
+    }	
+
+}
